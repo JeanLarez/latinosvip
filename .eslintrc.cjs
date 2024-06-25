4ebe540046e49687795ec69a99893cc6dc096579
JeanLarez <jeanlarez@gmail.com>
2024-06-24 22:27:38 -0300

    Add rules tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,15 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {node:true, browser: true, es2023: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:tailwindcss/recommended'
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'tailwindcss'],
   rules: {
     'react-refresh/only-export-components': 'warn',
     "react/prop-types": "off"
